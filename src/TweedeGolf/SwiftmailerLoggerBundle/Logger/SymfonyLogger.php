97af6dd3542bfd044bdf3ffbb468c97cacdfc576
Sören Helbig <soeren.helbig@projektmotor.de>
2020-02-01 12:31:51 +0100

    fixes data serialization in SymfonyLogger

--- src/TweedeGolf/SwiftmailerLoggerBundle/Logger/SymfonyLogger.php
+++ src/TweedeGolf/SwiftmailerLoggerBundle/Logger/SymfonyLogger.php
@@ -25,21 +25,44 @@ class SymfonyLogger implements LoggerInterface
         $this->logger = $logger;
     }
 
+
     /**
      * @param array $data
      */
     public function log(array $data)
     {
+        /** @var \Swift_Message $swiftMessage */
+        $swiftMessage = $data['message'];
+
         $this->logger->addInfo(\sprintf(
-            'from: %s; reply-to: %s; return-path: %s; to: %s; cc: %s; bcc: %s; subject: %s; date: %s',
-            $data['message']->getFrom(),
-            $data['message']->getReplyTo(),
-            $data['message']->getReturnPath(),
-            $data['message']->getTo(),
-            $data['message']->getCc(),
-            $data['message']->getBcc(),
+            'MAIL SENT - from: %s; reply-to: %s; return-path: %s; to: %s; cc: %s; bcc: %s; subject: %s; date: %s',
+            $this->getStringValue($swiftMessage->getFrom()),
+            $this->getStringValue($swiftMessage->getReplyTo()),
+            $this->getStringValue($swiftMessage->getReturnPath()),
+            $this->getStringValue($swiftMessage->getTo()),
+            $this->getStringValue($swiftMessage->getCc()),
+            $this->getStringValue($swiftMessage->getBcc()),
             $data['message']->getSubject(),
             $data['message']->getDate()->format('d.m.Y - H:i:s')
         ));
+    }
+
+    /**
+     * @param mixed $fieldValue
+     * @return string
+     */
+    private function getStringValue($fieldValue)
+    {
+        if (\is_string($fieldValue)) {
+            return $fieldValue;
+        } else if (\is_array($fieldValue)) {
+            $fromStrings = [];
+
+            foreach ($fieldValue as $prefix => $suffix) {
+                $fromStrings[] = (empty($suffix)) ? $prefix : $prefix.'<'.$suffix.'>';
+            }
+
+            return \implode(', ', $fromStrings);
+        }
     }
 }
